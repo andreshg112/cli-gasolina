95e69a05d2c5b99418b6f95cab631eab6a1026f9
andreshg112 <andreshg112@gmail.com>
2016-03-19 20:31:50 -0500

    Estructura organizada basado en buenas prácticas, funcionando.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # cli-gasolina
+Primer parcial - Tecnología Web II
+https://sites.google.com/site/tecweb2upc/notas
+
+Ingeniería de Sistemas
+Universidad Popular del Cesar
+
+Ver las buenas prácticas de la estructura de directorios en AngularJS (See Best practices of directories structure in AngularJS):
+https://scotch.io/tutorials/angularjs-best-practices-directory-structure
---
+++ app/app.routes.js
@@ -0,0 +1,18 @@
+(function() {
+    app.config(['$routeProvider',
+        function($routeProvider) {
+            $routeProvider
+                .when('/', {
+                    templateUrl: 'app/components/acerca/acercaView.html'
+                })
+                .when('/Registrar/', {
+                    templateUrl: 'app/components/registrar/registrarView.html'
+                })
+                .when('/Consultar/', {
+                    templateUrl: 'app/components/consultar/consultarView.html'
+                })
+                .when('/Acerca/', {
+                    templateUrl: 'app/components/acerca/acercaView.html'
+                });
+        }]);
+})();
