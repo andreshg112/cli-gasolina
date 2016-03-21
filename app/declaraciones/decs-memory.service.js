f9d82627095c245b1cfc2bebcf50aa17b47a4606
andreshg112 <andreshg112@gmail.com>
2016-03-21 17:55:01 -0500

    Guarda en localStorage.

--- app/declaraciones/decs-memory.service.js
+++ app/declaraciones/decs-memory.service.js
@@ -1,3 +1,4 @@
+//Almacena en memoria temporal. Se borran los datos al recargar la página.
 (function() {
     'use strict';
 
@@ -11,10 +12,10 @@
         this.all = all;
         this.save = save;
 
-        function all() {
-            return declaraciones;
-        }
+        function all() { return declaraciones; }
+        
         function save(declaracion) {
+            //Retorna true o false, si guardó o no, respectivamente.
             var cantidadAnterior = declaraciones.length;
             var cantidadNueva = declaraciones.push(declaracion);
             return cantidadAnterior < cantidadNueva;
